a332ed657ed65e4aab570a9e1b3973f0baad757e
ryulurala <ryulurala@gmail.com>
2022-10-11 20:20:33 +0900

    :sparkles: Fixed point 64bits, Vector Fixed Point

---
+++ Assets/Scripts/FixedMath/Fix64.cs
@@ -0,0 +1,268 @@
+using System;
+
+namespace FixedMath
+{
+    // [1(sign)] [47(integer part)] [16(decimal part)] 
+    public struct Fix64 : IComparable<Fix64>, IEquatable<Fix64>
+    {
+        readonly long _rawValue;
+
+        const int SHIFT = 16;
+        const int NBITS = 64;
+        const int NSQRT = 8;
+
+        const long ONE = 1L << SHIFT;
+
+        public static readonly Fix64 MaxValue = new Fix64(long.MaxValue);
+        public static readonly Fix64 MinValue = new Fix64(long.MinValue);
+        public static readonly Fix64 One = new Fix64(ONE);
+        public static readonly Fix64 Zero = new Fix64(0L);
+
+        static readonly Fix64 _onePointFive = One + (One >> 1);     // 1.5, for. InvSqrt
+
+        #region Constructors
+
+        Fix64(int value)
+        {
+            _rawValue = ((long)value) << SHIFT;     // for. decimal part
+        }
+
+        Fix64(long value)
+        {
+            _rawValue = value;
+        }
+
+        Fix64(float value)
+        {
+            _rawValue = (long)(value * ONE);
+        }
+
+        Fix64(double value)
+        {
+            _rawValue = (long)(value * ONE);
+        }
+
+        #endregion
+
+        #region Operators
+
+        public static Fix64 operator +(Fix64 a, Fix64 b)
+        {
+            long sum = a._rawValue + b._rawValue;
+#if !UNSAFE
+            if (((a._rawValue ^ sum) & (b._rawValue ^ sum) >> (NBITS - 1)) != 0)
+                sum = a._rawValue > 0 ? long.MaxValue : long.MinValue;
+#endif
+            return new Fix64(sum);
+        }
+
+        public static Fix64 operator -(Fix64 a, Fix64 b)
+        {
+            long diff = a._rawValue - b._rawValue;
+#if !UNSAFE
+            if (((~(a._rawValue ^ b._rawValue)) & (diff & a._rawValue)) < 0)
+                diff = a._rawValue > 0 ? long.MaxValue : long.MinValue;
+#endif
+            return new Fix64(diff);
+        }
+
+        public static Fix64 operator -(Fix64 value)
+        {
+            return value == MinValue ? MinValue : new Fix64(-value._rawValue);
+        }
+
+        public static Fix64 operator *(Fix64 a, Fix64 b)
+        {
+#if !UNSAFE
+            if (b._rawValue > ONE && a._rawValue > 2147483647L / b._rawValue)
+                throw new OverflowException();
+#endif
+            return new Fix64((a._rawValue * b._rawValue) >> SHIFT);
+        }
+
+        public static Fix64 operator /(Fix64 a, Fix64 b)
+        {
+            return new Fix64((a._rawValue << SHIFT) / b._rawValue);
+        }
+
+        public static Fix64 operator %(Fix64 a, Fix64 b)
+        {
+            return new Fix64(a._rawValue % b._rawValue);
+        }
+
+        public static bool operator ==(Fix64 a, Fix64 b)
+        {
+            return a._rawValue == b._rawValue;
+        }
+
+        public static bool operator !=(Fix64 a, Fix64 b)
+        {
+            return !(a._rawValue == b._rawValue);
+        }
+
+        public static bool operator >(Fix64 a, Fix64 b)
+        {
+            return a._rawValue > b._rawValue;
+        }
+
+        public static bool operator >=(Fix64 a, Fix64 b)
+        {
+            return a._rawValue >= b._rawValue;
+        }
+
+        public static bool operator <(Fix64 a, Fix64 b)
+        {
+            return a._rawValue < b._rawValue;
+        }
+
+        public static bool operator <=(Fix64 a, Fix64 b)
+        {
+            return a._rawValue <= b._rawValue;
+        }
+
+        public static Fix64 operator <<(Fix64 n, int shift)
+        {
+            return new Fix64(n._rawValue << shift);
+        }
+
+        public static Fix64 operator >>(Fix64 n, int shift)
+        {
+            return new Fix64(n._rawValue >> shift);
+        }
+
+        #endregion
+
+        #region Casting
+
+        public static explicit operator Fix64(int value)
+        {
+            return new Fix64(value);
+        }
+
+        public static explicit operator Fix64(long value)
+        {
+            return new Fix64(value << SHIFT);
+        }
+
+        public static explicit operator Fix64(float value)
+        {
+            return new Fix64(value);
+        }
+
+        public static explicit operator Fix64(double value)
+        {
+            return new Fix64(value);
+        }
+
+        public static explicit operator int(Fix64 value)
+        {
+            return (int)(value._rawValue >> SHIFT);
+        }
+
+        public static explicit operator long(Fix64 value)
+        {
+            return value._rawValue >> SHIFT;
+        }
+
+        public static explicit operator float(Fix64 value)
+        {
+            return (float)value._rawValue / ONE;
+        }
+
+        public static explicit operator double(Fix64 value)
+        {
+            return (double)value._rawValue / ONE;
+        }
+
+        #endregion
+
+        #region Inherited
+
+        public int CompareTo(Fix64 other)
+        {
+            return (int)(this - other);
+        }
+
+        public bool Equals(Fix64 other)
+        {
+            return this == other;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (obj is Fix64) ? (this == ((Fix64)obj)) : false;
+        }
+
+        public override int GetHashCode()
+        {
+            return _rawValue.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return ((double)_rawValue / ONE).ToString();
+        }
+
+        #endregion
+
+        #region Math
+
+        public static Fix64 Abs(Fix64 value)
+        {
+            long mask = value._rawValue >> (NBITS - 1);
+
+            return new Fix64((value._rawValue + mask) ^ mask);
+        }
+
+        public static Fix64 Clamp(Fix64 value, Fix64 min, Fix64 max)
+        {
+            return value > max ? max : value < min ? min : value;
+        }
+
+        public static Fix64 Max(Fix64 a, Fix64 b)
+        {
+            return a > b ? a : b;
+        }
+
+        public static Fix64 Min(Fix64 a, Fix64 b)
+        {
+            return a < b ? a : b;
+        }
+
+        public static Fix64 Sqrt(Fix64 value, int iterations)
+        {
+            if (value._rawValue < 0)
+                throw new ArithmeticException("Nagative value");
+
+            if (value._rawValue == 0)
+                return Fix64.Zero;
+
+            Fix64 result = value + Fix64.One >> 1;
+            for (uint i = 0; i < iterations; i++)
+                result = (result + (value / result)) >> 1;
+
+            if (result._rawValue < 0)
+                throw new ArithmeticException("Overflow");
+
+            return result;
+        }
+
+        public static Fix64 Sqrt(Fix64 value)
+        {
+            return Sqrt(value, NSQRT);
+        }
+
+        public static Fix64 InvSqrt(Fix64 value)
+        {
+            Fix64 half = value >> 1;
+
+            Fix64 result = half;
+            for (ushort i = 0; i < NSQRT; i++)
+                result *= _onePointFive - half * result * result;
+
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/FixedMath/Vector2Fix.cs
@@ -0,0 +1,231 @@
+namespace FixedMath
+{
+    public struct Vector2Fix
+    {
+        public Fix64 x;
+        public Fix64 y;
+
+        public static Vector2Fix Zero { get; } = new Vector2Fix(0, 0);
+        public static Vector2Fix One { get; } = new Vector2Fix(1, 1);
+
+        public static Vector2Fix Up { get; } = new Vector2Fix(0, 1);
+        public static Vector2Fix Down { get; } = new Vector2Fix(0, -1);
+        public static Vector2Fix Left { get; } = new Vector2Fix(-1, 0);
+        public static Vector2Fix Right { get; } = new Vector2Fix(1, 0);
+
+        public Fix64 SqrtMagnitude { get => DistanceSquared(this, Zero); }
+        public Fix64 Magnitude { get => Fix64.Sqrt(SqrtMagnitude); }
+
+        public Vector2Fix Normalized
+        {
+            get
+            {
+                Vector2Fix vec2F = new Vector2Fix(x, y);
+                vec2F.Normalize();
+
+                return vec2F;
+            }
+        }
+
+        #region Constructor
+
+        public Vector2Fix(Fix64 x, Fix64 y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Vector2Fix(int x, int y)
+        {
+            this.x = (Fix64)x;
+            this.y = (Fix64)y;
+        }
+
+        public Vector2Fix(UnityEngine.Vector2 v)
+        {
+            x = (Fix64)v.x;
+            y = (Fix64)v.y;
+        }
+
+        #endregion
+
+        #region Operators
+
+        public static bool operator ==(Vector2Fix v1, Vector2Fix v2)
+        {
+            return v1.x == v2.x && v1.y == v2.y;
+        }
+
+        public static bool operator !=(Vector2Fix v1, Vector2Fix v2)
+        {
+            return !(v1 == v2);
+        }
+
+        public static Vector2Fix operator +(Vector2Fix v1, Vector2Fix v2)
+        {
+            v1.x += v2.x;
+            v1.y += v2.y;
+
+            return v1;
+        }
+
+        public static Vector2Fix operator -(Vector2Fix v1, Vector2Fix v2)
+        {
+            v1.x -= v1.y;
+            v2.y -= v2.y;
+
+            return v1;
+        }
+
+        public static Vector2Fix operator -(Vector2Fix v)
+        {
+            v.x = -v.x;
+            v.y = -v.y;
+
+            return v;
+        }
+
+        public static Vector2Fix operator *(Vector2Fix v1, Vector2Fix v2)
+        {
+            v1.x *= v2.x;
+            v1.y *= v2.y;
+
+            return v1;
+        }
+
+        public static Vector2Fix operator *(Vector2Fix v, Fix64 f)
+        {
+            v.x *= f;
+            v.y *= f;
+
+            return v;
+        }
+
+        public static Vector2Fix operator *(Vector2Fix v, int i)
+        {
+            Fix64 fix = (Fix64)i;
+            v.x *= fix;
+            v.y *= fix;
+
+            return v;
+        }
+
+        public static Vector2Fix operator *(Vector2Fix v, float f)
+        {
+            Fix64 fix = (Fix64)f;
+            v.x *= fix;
+            v.y *= fix;
+
+            return v;
+        }
+
+        public static Vector2Fix operator /(Vector2Fix v1, Vector2Fix v2)
+        {
+            v1.x /= v2.x;
+            v1.y /= v2.y;
+
+            return v1;
+        }
+
+        public static Vector2Fix operator /(Vector2Fix v, int i)
+        {
+            Fix64 fix = (Fix64)i;
+            v.x /= fix;
+            v.y /= fix;
+
+            return v;
+        }
+
+        public static Vector2Fix operator /(Vector2Fix v, float f)
+        {
+            Fix64 fix = (Fix64)f;
+            v.x /= fix;
+            v.y /= fix;
+
+            return v;
+        }
+
+        #endregion
+
+        #region Math
+
+        public static Fix64 DistanceSquared(Vector2Fix v1, Vector2Fix v2)
+        {
+            Fix64 x = v1.x - v2.x;
+            Fix64 y = v1.y - v2.y;
+
+            return x * x + y * y;
+        }
+
+        public static Fix64 Distance(Vector2Fix v1, Vector2Fix v2)
+        {
+            return Fix64.Sqrt(DistanceSquared(v1, v2));
+        }
+
+        public void Normalize()
+        {
+            Fix64 sqrMag = SqrtMagnitude;
+            Fix64 invMag = (sqrMag > Fix64.Zero) ? Fix64.InvSqrt(sqrMag) : Fix64.Zero;
+
+            x *= invMag;
+            y *= invMag;
+        }
+
+        public static Vector2Fix Abs(Vector2Fix v)
+        {
+            return new Vector2Fix(Fix64.Abs(v.x), Fix64.Abs(v.y));
+        }
+
+        public static Vector2Fix Clamp(Vector2Fix value, Vector2Fix min, Vector2Fix max)
+        {
+            value.x = Fix64.Clamp(value.x, min.x, max.x);
+            value.y = Fix64.Clamp(value.y, min.y, max.y);
+
+            return value;
+        }
+
+        public static Fix64 Dot(Vector2Fix v1, Vector2Fix v2)
+        {
+            return v1.x * v2.x + v1.y * v2.y;
+        }
+
+        public static Vector2Fix Max(Vector2Fix v1, Vector2Fix v2)
+        {
+            return new Vector2Fix(Fix64.Max(v1.x, v2.x), Fix64.Max(v1.y, v2.y));
+        }
+
+        public static Vector2Fix Min(Vector2Fix v1, Vector2Fix v2)
+        {
+            return new Vector2Fix(Fix64.Min(v1.x, v2.x), Fix64.Max(v1.y, v2.y));
+        }
+
+        #endregion
+
+        #region Casting
+
+        public static explicit operator UnityEngine.Vector2(Vector2Fix value)
+        {
+            return new UnityEngine.Vector2((float)value.x, (float)value.y);
+        }
+
+        #endregion
+
+        #region Inherited
+        public override bool Equals(object obj)
+        {
+            return obj is Vector2Fix ? this == (Vector2Fix)obj : false;
+        }
+
+        public override int GetHashCode()
+        {
+            return x.GetHashCode() + y.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"({x}, {y})";
+        }
+        #endregion
+    }
+}
+
