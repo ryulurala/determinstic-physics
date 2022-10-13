4c4c9e46d8c1d6a3961c2dd0f22e06052ddc2068
ryulurala <ryulurala@gmail.com>
2022-10-13 13:41:23 +0900

    :bug: Solved Vector2Fix Opertator - bug

--- Assets/Scripts/FixedMath/Vector2Fix.cs
+++ Assets/Scripts/FixedMath/Vector2Fix.cs
@@ -79,8 +79,8 @@ namespace FixedMath
 
         public static Vector2Fix operator -(Vector2Fix v1, Vector2Fix v2)
         {
-            v1.x -= v1.y;
-            v2.y -= v2.y;
+            v1.x -= v2.x;
+            v1.y -= v2.y;
 
             return v1;
         }
